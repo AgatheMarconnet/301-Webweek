837506bc93e93650f84a29fc400ec1fcdb1fa3b4
Maxxiaa <maiaas.13005@gmail.com>
2026-01-13 16:49:33 +0100

    ajout du footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,6 +2,30 @@
 
 ?>
 
-<!--HEADER-->
-<header>
-</header>
+<!--footer-->
+<footer>
+    <div class="footer.logo">
+    <a href="index.php">
+      <img src="logo.png" alt="Logo" />
+    </a>
+  </div>
+  <nav class="footer.menu">
+    <ul>
+      <li><a href="association.php">Association</a></li>
+      <li><a href="cours.php">Cours</a></li>
+      <li><a href="histoire.php">Histoire</a></li>
+      <li><a href="contact.php">Contact</a></li>
+    </ul>
+  </nav>
+  <div class="footer.address">
+    <p>1, avenue de Bonneville, 43000 Aiguilhe</p>
+  </div>
+  <div class="footer.rs">
+    <a href="https://www.facebook.com" target="_blank">
+      <img src="facebook.png" alt="Facebook" />
+    </a>
+    <a href="https://www.instagram.com" target="_blank"><!-- target blank ça permet d'ouvrir l'onglet du réseu social sans quitter le site -->
+      <img src="instagram.png" alt="Instagram" />
+    </a>
+  </div>
+</footer>
