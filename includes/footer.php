6791c63aba2211893beaa26c64afaceb6550324b
Maxxiaa <maiaas.13005@gmail.com>
2026-01-14 11:34:36 +0100

    footer fait sur index, ajout des logo RS, modification dans le css

--- includes/footer.php
+++ includes/footer.php
@@ -1,29 +1,2 @@
 <?php 
-
 ?>
-<footer>
-    <div class="footer-logo">
-    <a href="index.php">
-      <img src="../img/logo.jpg" alt="Logo" />
-    </a>
-  </div>
-  <nav class="footer-menu">
-    <ul>
-      <li><a href="../pages/association.php">Association</a></li>
-      <li><a href="../pages/cours.php">Cours</a></li>
-      <li><a href="../pages/histoire.php">Histoire</a></li>
-      <li><a href="../pages/contact.php">Contact</a></li>
-    </ul>
-  </nav>
-  <div class="footer-address">
-    <p>1, avenue de Bonneville, 43000 Aiguilhe</p>
-  </div>
-  <div class="footer-rs">
-    <a href="https://www.facebook.com" target="_blank">
-      <img src="facebook.png" alt="Facebook" />
-    </a>
-    <a href="https://www.instagram.com" target="_blank"><!-- target blank ça permet d'ouvrir l'onglet du réseu social sans quitter le site -->
-      <img src="instagram.png" alt="Instagram" />
-    </a>
-  </div>
-</footer>
--- index.php
+++ index.php
@@ -4,9 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
-<link rel="stylesheet" href="css/styles.css">
+<link rel="stylesheet" href="css/style.css"> 
 </head>
 <body>
-    <?php include 'footer.php'; ?>
+<footer>
+    <div class="footer-logo">
+    <a href="index.php">
+      <img src="img/logo.jpg" alt="Logo" />
+    </a>
+  </div>
+  <nav class="footer-menu">
+    <ul>
+      <li><a href="../pages/association.php">Association</a></li>
+      <li><a href="../pages/cours.php">Cours</a></li>
+      <li><a href="../pages/histoire.php">Histoire</a></li>
+      <li><a href="../pages/contact.php">Contact</a></li>
+    </ul>
+  </nav>
+  <div class="footer-address">
+    <p>1, avenue de Bonneville, 43000 Aiguilhe</p>
+  </div>
+  <div class="footer-rs">
+    <a href="https://www.facebook.com" target="_blank">
+      <img src="img/Facebook_logo_2023.png" alt="logo Facebook" />
+    </a>
+    <a href="https://www.instagram.com" target="_blank"><!-- target blank ça permet d'ouvrir l'onglet du réseu social sans quitter le site -->
+      <img src="Instagram_icon.png" alt="logo Instagram" />
+    </a>
+  </div>
+</footer>
 </body>
 </html>
