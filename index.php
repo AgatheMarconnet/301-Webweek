054803c96e7adc7e2061e9238fd9f358847a0bae
Maxxiaa <maiaas.13005@gmail.com>
2026-01-14 15:01:17 +0100

    finission du footer

--- index.php
+++ index.php
@@ -15,22 +15,25 @@
   </div>
   <nav class="footer-menu">
     <ul>
-      <li><a href="../pages/association.php">Association</a></li>
-      <li><a href="../pages/cours.php">Cours</a></li>
-      <li><a href="../pages/histoire.php">Histoire</a></li>
-      <li><a href="../pages/contact.php">Contact</a></li>
+      <li><a href="pages/association.php">Association</a></li>
+      <li><a href="pages/cours.php">Cours</a></li>
+      <li><a href="pages/histoire.php">Histoire</a></li>
+      <li><a href="pages/contact.php">Contact</a></li>
     </ul>
   </nav>
-  <div class="footer-address">
-    <p>1, avenue de Bonneville, 43000 Aiguilhe</p>
-  </div>
-  <div class="footer-rs">
-    <a href="https://www.facebook.com" target="_blank">
-      <img src="img/Facebook_logo_2023.png" alt="logo Facebook" />
-    </a>
-    <a href="https://www.instagram.com" target="_blank"><!-- target blank ça permet d'ouvrir l'onglet du réseu social sans quitter le site -->
-      <img src="Instagram_icon.png" alt="logo Instagram" />
-    </a>
+  <div class="footer-droite">
+
+    <div class="footer-address">
+        <p>1, avenue de Bonneville, 43000 Aiguilhe</p>
+    </div>
+    <div class="footer-rs">
+        <a href="https://www.facebook.com/people/A%C3%AFkido-Le-Puy-en-Velay/100070297455766/" target="_blank">
+        <img src="img/Facebook_logo_2023.png" alt="logo Facebook" />
+        </a>
+        <a href="https://www.instagram.com/aikidolepuyenvelay/" target="_blank"><!-- target blank ça permet d'ouvrir l'onglet du réseu social sans quitter le site -->
+        <img src="img/Instagram_icon.png" alt="logo Instagram" />
+        </a>
+    </div>
   </div>
 </footer>
 </body>
