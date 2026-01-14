d9562666b9103827303727d61d4c61b1850bf6f4
AgatheMarconnet <agathe.marconnet@etu.uca.fr>
2026-01-14 11:35:36 +0100

    Requêtes + affichage simple des stages depuis la BDD

--- pages/articlestage.php
+++ pages/articlestage.php
@@ -1,3 +1,20 @@
+<?php 
+include_once '../classes/database.php';
+include_once '../classes/stage.php';
+
+$stageChoisi = null;
+
+//Faire appel à la classe database
+$db = database::getInstance('aikido'); 
+
+// Requete pour récupérer le stage 
+$sql = "SELECT * FROM stage WHERE idStage = 31";
+    
+$resultats = $db->getObjects($sql, 'Stage', []);
+$stageChoisi = $resultats[0];
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -6,6 +23,17 @@
     <title>Stage</title>
 </head>
 <body>
+    <main>
+
+            <div>
+                <img src="<?php echo $stageChoisi->getImage(); ?>" alt="Affiche du stage" style="width:200px;"/>
+                
+                <h1><?php echo $stageChoisi->getNom(); ?></h1>
+                
+                <p><strong>Dates :</strong> Du <?php echo $stageChoisi->getDateDebut(); ?> au <?php echo $stageChoisi->getDateFin(); ?></p>
+                <p><strong>Horaires :</strong> <?php echo $stageChoisi->getHoraires(); ?></p>
+                <p><strong>Tarif :</strong> <?php echo $stageChoisi->getTarif(); ?> €</p>
+    </main>
     
 </body>
 </html>
--- pages/stagecarte.php
+++ pages/stagecarte.php
@@ -1,3 +1,18 @@
+<?php 
+include_once '../classes/database.php';
+include_once '../classes/stage.php';
+
+//Faire appel à la classe database
+$db = database::getInstance('aikido'); 
+
+// Requete pour récupérer le stage 
+$sql = "SELECT * FROM stage";
+    
+$resultats = $db->getObjects($sql, 'Stage', []);
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -6,6 +21,21 @@
     <title>Stages</title>
 </head>
 <body>
+    <main>
+      <!-- Afficher chacuns des stages-->
+      <?php foreach ($resultats as $unStage) : ?>
+       <div class="carte">
+            <!-- rediriger vers le stage en fonction de l'id-->
+            <a href="stagecarte.php?id=<?php echo $unStage->getId(); ?>">
+                <img src="<?php echo $unStage->getImage(); ?>" alt="Affiche du stage" />
+            </a>
+            <h3><?php echo $unStage->getNom(); ?></h3>
+            <p>Ville : <?php echo $unStage->getHoraires(); ?></p> 
+            <p>Tarif : <?php echo $unStage->getTarif(); ?> €</p>
+        </div>
+      <?php endforeach; ?>
+    </main>
+    </main>
     
 </body>
 </html>
