91a31d00d6ff64172102f0ee0b28b666be06a204
Maxxiaa <maiaas.13005@gmail.com>
2026-01-14 10:50:26 +0100

    css footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,27 +1,29 @@
 <?php 
 
 ?>
-
+<head>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
 <!--footer-->
 <link rel="stylesheet" href="css/styles.css">
 <footer>
-    <div class="footer.logo">
+    <div class="footer-logo">
     <a href="index.php">
       <img src="../img/logo.jpg" alt="Logo" />
     </a>
   </div>
-  <nav class="footer.menu">
+  <nav class="footer-menu">
     <ul>
-      <li><a href="association.php">Association</a></li>
-      <li><a href="cours.php">Cours</a></li>
-      <li><a href="histoire.php">Histoire</a></li>
-      <li><a href="contact.php">Contact</a></li>
+      <li><a href="/301-Webweek/pages/association.php">Association</a></li>
+      <li><a href="/301-Webweek/pages/cours.php">Cours</a></li>
+      <li><a href="/301-Webweek/pages/histoire.php">Histoire</a></li>
+      <li><a href="/301-Webweek/pages/contact.php">Contact</a></li>
     </ul>
   </nav>
-  <div class="footer.address">
+  <div class="footer-address">
     <p>1, avenue de Bonneville, 43000 Aiguilhe</p>
   </div>
-  <div class="footer.rs">
+  <div class="footer-rs">
     <a href="https://www.facebook.com" target="_blank">
       <img src="facebook.png" alt="Facebook" />
     </a>
