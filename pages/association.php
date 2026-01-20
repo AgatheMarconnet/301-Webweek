df8dc85f4c3d58cf1b806d6fddcb2fb99a52474b
AgatheMarconnet <agathe.marconnet@etu.uca.fr>
2026-01-20 10:55:51 +0100

    Indentations pages

--- pages/association.php
+++ pages/association.php
@@ -5,60 +5,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
     <title>Association</title>
- 
+    <script src="../js/script.js"></script> 
 </head>
 <body>
     <?php
-    //J'utilise la variable path pour que le chemin s'adapte en fonction de la page (ce ne sont pas les même chemin si on vient de index ou d'une autre page)
-    $path = "../";
-    include '../includes/header.php'; 
+        //J'utilise la variable path pour que le chemin s'adapte en fonction de la page (ce ne sont pas les même chemin si on vient de index ou d'une autre page)
+        $path = "../";
+        include '../includes/header.php'; 
     ?>
-   <h1 class="titre-assoc">Thomas Gavory</h1>
-   <h2 class="sous-titre-assoc"> 6ème DAN</h2>
-   <p class="paragraphe-assoc"> Diplômé d'État</p>
-   
-<div class="Thomas-presentation">
-    <p><strong>Thomas Gavory</strong> participe à de nombreux événements majeurs, notamment au Festival des Arts  Martiaux de Bercy, où il intervient comme Uke auprès de figures reconnues de l’aïkido : Christian Tissier Shihan (2001, 2003, 2004, 2007), Marc Bachraty Senseï (2011) et Yoko Okamoto Senseï (2012). <br>
-    <br>
-    En 2012, il se rend au Japon avec la délégation de la FFAAA et assiste Micheline Tissier Senseï lors du 11ᵉ Congrès de la Fédération Internationale d’Aïkido. <br>
-    Après avoir enseigné à Noisy-le-Grand de 1994 à 2010 aux côtés de Pierre Helley, Thomas Gavory quitte la région parisienne pour s’installer au Puy-en-Velay. <br>
-    <br>
-    Aujourd’hui professeur au <strong>club d’Aïkido du Puy-en-Velay</strong>, il anime régulièrement des stages dans d’autres clubs auvergnats (Saint-Flour, CUC de Clermont-Ferrand…). Depuis 2013, il est également l’assistant d’Alain Royer (7ᵉ dan), directeur technique régional de la Ligue Auvergne, avec qui il coanime les stages de ligue.<br>
-</p>
-</div>
-<h1>Galerie</h1>
-    <div class="galerie_photoscouleur">
-        <img src="../img/photo_cours.png" alt="photo de cours">
-        <img src="../img/photo-prise.png" alt="photo prise aikido">
-        <img src="../img/photo-sincline.png" alt="photo de thomas gavory et un de ses élève qui se prosterne">
-        <img src="../img/photo-prise-sol.png" alt="photo d'une prise au sol">
-        <img src="../img/photo-prise-tournoi.png" alt ="photo d'une pris d'aikido à un tournoi">
-        <img src="../img/photo-cours-tokyo.png" alt="photo d'un cours à tokyo">
-        <img src="../img/photo_cours-endo-sensei.png" alt="photo avec endo sensei">
-        <img src="../img/photo-groupe_1.png" alt="photo d'un groupe avec Thomas Gavory">
-        <img src="../img/photo-groupe_2.png" alt="photo de groupe avec Thomas Gavory">
-    </div>
-
-   <div class="test-carrousel">
-        <img id="imageAffiche" src="../img/photo-noir-blanc-couteau.png" style="width: 100%;">
+    <main>
+        <h1 class="titre-assoc">Thomas Gavory</h1>
+        <h2 class="sous-titre-assoc"> 6ème DAN</h2>
+        <p class="paragraphe-assoc"> Diplômé d'État</p>
     
-        <button id="btnSuivant" class="bouton">Image Suivante</button>
+        <div class="Thomas-presentation">
+            <p>
+                <strong>Thomas Gavory</strong> participe à de nombreux événements majeurs, notamment au Festival des Arts  Martiaux de Bercy, où il intervient comme Uke auprès de figures reconnues de l’aïkido : Christian Tissier Shihan (2001, 2003, 2004, 2007), Marc Bachraty Senseï (2011) et Yoko Okamoto Senseï (2012). <br>
+                <br>
+                En 2012, il se rend au Japon avec la délégation de la FFAAA et assiste Micheline Tissier Senseï lors du 11ᵉ Congrès de la Fédération Internationale d’Aïkido. <br>
+                Après avoir enseigné à Noisy-le-Grand de 1994 à 2010 aux côtés de Pierre Helley, Thomas Gavory quitte la région parisienne pour s’installer au Puy-en-Velay. <br>
+                <br>
+                Aujourd’hui professeur au <strong>club d’Aïkido du Puy-en-Velay</strong>, il anime régulièrement des stages dans d’autres clubs auvergnats (Saint-Flour, CUC de Clermont-Ferrand…). Depuis 2013, il est également l’assistant d’Alain Royer (7ᵉ dan), directeur technique régional de la Ligue Auvergne, avec qui il coanime les stages de ligue.<br>
+            </p>
+        </div>
+        <h1>Galerie</h1>
+        <div class="galerie_photoscouleur">
+            <img src="../img/photo_cours.png" alt="photo de cours">
+            <img src="../img/photo-prise.png" alt="photo prise aikido">
+            <img src="../img/photo-sincline.png" alt="photo de thomas gavory et un de ses élève qui se prosterne">
+            <img src="../img/photo-prise-sol.png" alt="photo d'une prise au sol">
+            <img src="../img/photo-prise-tournoi.png" alt ="photo d'une pris d'aikido à un tournoi">
+            <img src="../img/photo-cours-tokyo.png" alt="photo d'un cours à tokyo">
+            <img src="../img/photo_cours-endo-sensei.png" alt="photo avec endo sensei">
+            <img src="../img/photo-groupe_1.png" alt="photo d'un groupe avec Thomas Gavory">
+            <img src="../img/photo-groupe_2.png" alt="photo de groupe avec Thomas Gavory">
+        </div>
 
-    </div>
+        <div class="test-carrousel">
+            <img id="imageAffiche" src="../img/photo-noir-blanc-couteau.png" style="width: 100%;">
+            <button id="btnSuivant" class="bouton">Image Suivante</button>
+        </div>
 
-    <div class="video-aikido">
-        <iframe width="200" height="170" src="https://www.youtube.com/embed/Y8is-ihz_qI?si=C64AoFkzDNRogifY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <iframe width="200" height="170" src="https://www.youtube.com/embed/Y8is-ihz_qI?si=WYp2VOG539eUcCH5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <iframe width="200" height="170" src="https://www.youtube.com/embed/-aOPtRwIcXg?si=PFKT952n4CUpYXSX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <iframe width="200" height="170" src="https://www.youtube.com/embed/GX91U2t4zmI?si=eGrg_0nDaKoo6P4q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <iframe width="200" height="170" src="https://www.youtube.com/embed/0mJSHEyyKZg?si=1MISYQRsLCXxz2QU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-
-    </div>  
-<script src="../js/script.js"></script>  
-<footer>
-    <?php
-    include '../includes/footer.php';
-    ?>
-</footer>     
+        <div class="video-aikido">
+            <iframe width="200" height="170" src="https://www.youtube.com/embed/Y8is-ihz_qI?si=C64AoFkzDNRogifY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="200" height="170" src="https://www.youtube.com/embed/Y8is-ihz_qI?si=WYp2VOG539eUcCH5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="200" height="170" src="https://www.youtube.com/embed/-aOPtRwIcXg?si=PFKT952n4CUpYXSX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="200" height="170" src="https://www.youtube.com/embed/GX91U2t4zmI?si=eGrg_0nDaKoo6P4q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="200" height="170" src="https://www.youtube.com/embed/0mJSHEyyKZg?si=1MISYQRsLCXxz2QU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div> 
+    </main>  
+    <?php include '../includes/footer.php';?>  
 </body>
 </html>
--- pages/modifierAvis.php
+++ pages/modifierAvis.php
@@ -2,10 +2,8 @@
 session_start();
 include_once '../classes/database.php';
 include_once '../classes/avis.php';
-
 //Recuperer l'id de l'avis à modifier
 $idAvis = $_POST['idAvisActuel'];
-
 $db = database::getInstance('aikido');
 //requete pour recupérer toutes les infos d'un avis 
 $resultats = $db->getObjects("SELECT * FROM avis WHERE idAvis = $idAvis", 'Avis', []);
@@ -22,43 +20,47 @@ $unAvis = $resultats[0];
 </head>
 <body>
     <?php 
-    //J'utilise la variable path pour que le chemin s'adapte en fonction de la page (ce ne sont pas les même chemin si on vient de index ou d'une autre page)
-    $path = "../";
-    include '../includes/header.php';?> 
-    
+        //J'utilise la variable path pour que le chemin s'adapte en fonction de la page (ce ne sont pas les même chemin si on vient de index ou d'une autre page)
+        $path = "../";
+        include '../includes/header.php';
+    ?> 
     <main>
         <!-- Si l'utilisateur est connecté en tant qu'admin il peut modifier-->
-        <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true){?>
-        <h1>Modifier l'avis de <?php echo $unAvis->getNomAvis()?></h1>
-        <!--Formulaire pour les changements remplis avec les informations de l'avis-->
-        <!-- Renvoie vers updateAvis/php pour faire la requete dans la BDD-->
-        <form action="../api/updateAvis.php" method="POST">
-            <input type="hidden" name="idAvisActuel" value="<?php echo $unAvis->getIdAvis() ?>">
-            <label class="titre">Nom: </label> <br>
-            <input type="text" name="nomAvis" class="champ" value="<?php echo $unAvis->getNomAvis() ?>"><br><br>
-            <label class="titre">Titre: </label><br>
-            <input type="text" name="titreAvis" class="champ" value="<?php echo $unAvis->getTitreAvis() ?>"><br><br>
-            
-            <label class="titre">Note :</label><br>
-            <select id="noteAvis" name="noteAvis" class="champ" required>
-                <option value="5">5</option>
-                <option value="4">4</option>
-                <option value="3">3</option>
-                <option value="2">2</option>
-                <option value="1">1</option>
-            </select> <br><br>
-            <label class="titre">Commentaire: </label> <br>
-            <textarea name="descriptionAvis" class="champ" id="message"><?php echo $unAvis->getDescriptionAvis() ?></textarea><br> <br>
-            <button type="submit" class="bouton">Enregistrer les changements</button>
-        </form>
-        <?php }
-        //Si l'utilisateur n'est pas connecté en tant qu'admin, il ne peut pas modifier
-        else {
-            echo "Vous devez être connecté comme administrateur pour modifier ou supprimer un avis"; ?>
-            <form action="connexion.php" method="POST">
-                <button type="submit" class="btn">Se connecter</button>
-            </form>
-            <?php } ?>
+        <?php 
+            if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true){
+        ?>
+                <h1>Modifier l'avis de <?php echo $unAvis->getNomAvis()?></h1>
+                <!--Formulaire pour les changements remplis avec les informations de l'avis-->
+                <!-- Renvoie vers updateAvis/php pour faire la requete dans la BDD-->
+                <form action="../api/updateAvis.php" method="POST">
+                    <input type="hidden" name="idAvisActuel" value="<?php echo $unAvis->getIdAvis() ?>">
+                    <label class="titre">Nom: </label> <br>
+                    <input type="text" name="nomAvis" class="champ" value="<?php echo $unAvis->getNomAvis() ?>"><br><br>
+                    <label class="titre">Titre: </label><br>
+                    <input type="text" name="titreAvis" class="champ" value="<?php echo $unAvis->getTitreAvis() ?>"><br><br>
+                    <label class="titre">Note :</label><br>
+                    <select id="noteAvis" name="noteAvis" class="champ" required>
+                        <option value="5">5</option>
+                        <option value="4">4</option>
+                        <option value="3">3</option>
+                        <option value="2">2</option>
+                        <option value="1">1</option>
+                    </select> <br><br>
+                    <label class="titre">Commentaire: </label> <br>
+                    <textarea name="descriptionAvis" class="champ" id="message"><?php echo $unAvis->getDescriptionAvis() ?></textarea><br> <br>
+                    <button type="submit" class="bouton">Enregistrer les changements</button>
+                </form>
+        <?php 
+            }
+            //Si l'utilisateur n'est pas connecté en tant qu'admin, il ne peut pas modifier
+            else {
+                echo "Vous devez être connecté comme administrateur pour modifier ou supprimer un avis"; ?>
+                <form action="connexion.php" method="POST">
+                    <button type="submit" class="btn">Se connecter</button>
+                </form>
+        <?php 
+            } 
+        ?>
     </main>
     <?php include '../includes/footer.php';?> 
 </body>
